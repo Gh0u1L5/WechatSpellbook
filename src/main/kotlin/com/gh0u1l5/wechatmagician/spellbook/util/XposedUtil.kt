72e53a94d58c65cdd45b80ad5af4b63f852e9958
Emerson Lin <Em3rs0n@yahoo.com>
2018-03-29 04:22:11 -0400

    Improve the stability of XposedUtil.kt

--- src/main/kotlin/com/gh0u1l5/wechatmagician/spellbook/util/XposedUtil.kt
+++ src/main/kotlin/com/gh0u1l5/wechatmagician/spellbook/util/XposedUtil.kt
@@ -1,8 +1,10 @@
 package com.gh0u1l5.wechatmagician.spellbook.util
 
+import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
 import com.gh0u1l5.wechatmagician.spellbook.base.Hooker
+import com.gh0u1l5.wechatmagician.spellbook.util.BasicUtil.tryAsynchronously
 import com.gh0u1l5.wechatmagician.spellbook.util.BasicUtil.tryVerbosely
 
 object XposedUtil {
@@ -10,10 +12,27 @@ object XposedUtil {
     private val hookerHandlerThread = HandlerThread("HookerHandler").apply { start() }
     private val hookerHandler: Handler = Handler(hookerHandlerThread.looper)
 
+    /**
+     * Hooks functions in suitable strategy for corresponding API levels. NOTE: for Android 7.X or
+     * later, multi-thread causes unexpected crashes with WeXposed, so we drop this feature for now.
+     *
+     * @param hook the callback function that actually hook functions using Xposed.
+     */
+    private fun tryHook(hook: () -> Unit) {
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> tryVerbosely { hook() }
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> tryAsynchronously { hook() }
+            else -> tryAsynchronously { try { hook() } catch (t: Throwable) { /* Ignore */ } }
+        }
+    }
+
     fun postHooker(hooker: Hooker) {
         hookerHandler.post {
-            if (!hooker.hasHooked) {
-                tryVerbosely {
+            tryHook {
+                synchronized(hooker) {
+                    if (hooker.hasHooked) {
+                        return@tryHook
+                    }
                     hooker.hook()
                     hooker.hasHooked = true
                 }
